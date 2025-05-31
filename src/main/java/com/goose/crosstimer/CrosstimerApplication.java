fc79aa5e25a7241884c3059011ac443b49c41f03
Jeong TaeHee <th1563@naver.com>
2025-06-01 00:37:11 +0900

    crosstimer init

---
+++ src/main/java/com/goose/crosstimer/CrosstimerApplication.java
@@ -0,0 +1,13 @@
+package com.goose.crosstimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrosstimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrosstimerApplication.class, args);
+    }
+
+}
