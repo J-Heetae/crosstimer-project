e1efe5ba4cc54a152b46af4d4c1ca6320b50d3d7
Jeong TaeHee <th1563@naver.com>
2025-06-10 18:42:21 +0900

    Initial commit

---
+++ app/src/main/java/com/goose/crosstimerapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.goose.crosstimerapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+//안드로이드 앱의 한 화면(엑티비티)를 정의하는 클래스
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
