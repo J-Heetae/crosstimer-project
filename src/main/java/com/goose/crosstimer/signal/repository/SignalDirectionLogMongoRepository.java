81506a9f9115cd3430a88de371691f0e8ec4191c
Jeong TaeHee <th1563@naver.com>
2025-06-21 02:51:31 +0900

    refactor: 불필요한 클래스 제거

--- src/main/java/com/goose/crosstimer/signal/repository/SignalDirectionLogMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.goose.crosstimer.signal.repository;
-
-import com.goose.crosstimer.signal.domain.SignalDirectionLog;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SignalDirectionLogMongoRepository extends MongoRepository<SignalDirectionLog, String> {
-}
