95cd96511ef167f052a54acddd945a3d7a3bf697
Jeong TaeHee <th1563@naver.com>
2025-07-02 16:14:25 +0900

    refactor: SignalCacheRepositoryImpl, SignalCacheRepositoryCustom.java 제거

--- src/main/java/com/goose/crosstimer/signal/repository/SignalCacheRepository.java
+++ src/main/java/com/goose/crosstimer/signal/repository/SignalCacheRepository.java
@@ -3,5 +3,5 @@ package com.goose.crosstimer.signal.repository;
 import com.goose.crosstimer.signal.domain.SignalCache;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SignalCacheRepository extends CrudRepository<SignalCache, String>, SignalCacheRepositoryCustom {
+public interface SignalCacheRepository extends CrudRepository<SignalCache, String> {
 }
